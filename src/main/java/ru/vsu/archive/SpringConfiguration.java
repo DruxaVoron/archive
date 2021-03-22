f6bde5a7336a94935187971ecf7dbbae1d992553
andrew <avor.2000@gmail.com>
2021-03-22 20:06:54 +0300

    add spring configuration class

---
+++ src/main/java/ru/vsu/archive/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package ru.vsu.archive;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.vsu.archive")
+public class SpringConfiguration {
+
+
+}
